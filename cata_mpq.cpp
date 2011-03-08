761fad50450e869ca6af3f1b848f645c8c0c0de1
Magnus Osterlind <dooz@spotify.com>
2011-03-08 07:54:08 +0100

    a little refactoring, and started integrating pklib stuff

--- cata_mpq.cpp
+++ cata_mpq.cpp
@@ -6,9 +6,21 @@
 #include <limits>
 #include <set>
 #include "md5.h"
+#include "pklib.h"
 
 using namespace std;
 using namespace boost;
+
+// make some less annoying types
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
 
 // infos from http://wiki.devklog.net/index.php?title=The_MoPaQ_Archive_Format
 
@@ -245,95 +257,7 @@ struct TMPQBetTable
 
 #pragma pack(pop)
 
-uint32_t dwCryptTable[0x500];
-
-// The encryption and hashing functions use a number table in their procedures.
-// This table must be initialized before the functions are called the first time.
-void init_crypt_table()
-{
-	uint32_t seed = 0x00100001;
-
-	for (int index1 = 0; index1 < 0x100; index1++) {
-		for (int index2 = index1, i = 0; i < 5; i++, index2 += 0x100) {
-			uint32_t temp1, temp2;
-
-			seed  = (seed * 125 + 3) % 0x2AAAAB;
-			temp1 = (seed & 0xFFFF) << 0x10;
-
-			seed  = (seed * 125 + 3) % 0x2AAAAB;
-			temp2 = (seed & 0xFFFF);
-
-			dwCryptTable[index2] = (temp1 | temp2);
-		}
-	}
-}
-
-void decrypt_data(void *lpbyBuffer, uint32_t dwLength, uint32_t dwKey)
-{
-	uint32_t *lpdwBuffer = (uint32_t *)lpbyBuffer;
-	uint32_t seed = 0xEEEEEEEEL;
-	uint32_t ch;
-
-	dwLength /= sizeof(uint32_t);
-
-	while(dwLength-- > 0) {
-		seed += dwCryptTable[0x400 + (dwKey & 0xFF)];
-		ch = *lpdwBuffer ^ (dwKey + seed);
-
-		dwKey = ((~dwKey << 0x15) + 0x11111111L) | (dwKey >> 0x0B);
-		seed = ch + seed + (seed << 5) + 3;
-
-		*lpdwBuffer++ = ch;
-	}
-}
-
-// Different types of hashes to make with hash_string
-enum HashType {
-	HashTypeTableOffset = 0,
-	HashTypeMethodA = 1,
-	HashTypeMethodB = 2,
-	HashTypeBlockTable = 3,
-};
-/*
-#define MPQ_HASH_TABLE_OFFSET	0
-#define MPQ_HASH_NAME_A	1
-#define MPQ_HASH_NAME_B	2
-#define MPQ_HASH_FILE_KEY	3
-*/
-// Based on code from StormLib.
-uint32_t hash_string(const char *lpszString, uint32_t dwHashType)
-{
-	uint32_t seed1 = 0x7FED7FEDL;
-	uint32_t seed2 = 0xEEEEEEEEL;
-	while (*lpszString) {
-		int ch = toupper(*lpszString++);
-
-		seed1 = dwCryptTable[(dwHashType * 0x100) + ch] ^ (seed1 + seed2);
-		seed2 = ch + seed1 + seed2 + (seed2 << 5) + 3;
-	}
-	return seed1;
-}
-
 #define BLOCK_OFFSET_ADJUSTED_KEY 0x00020000L
-
-uint32_t compute_file_key(const char *lpszFilePath, const BlockTableEntry &blockEntry, uint32_t nArchiveOffset)
-{
-	// Find the file name part of the path
-	const char *lpszFileName = strrchr(lpszFilePath, '\\');
-	if (lpszFileName)
-		lpszFileName++;	// Skip the '\'
-	else
-	lpszFileName = lpszFilePath;
-
-	// Hash the name to get the base key
-	uint32_t nFileKey = hash_string(lpszFileName, HashTypeBlockTable);
-
-	// Offset-adjust the key if necessary
-	if (blockEntry.flags & kBtFlagsAdjustedKey)
-		nFileKey = (nFileKey + blockEntry.ofs) ^ blockEntry.file_size;
-
-	return nFileKey;
-}
 
 bool find_header(const FileReader &f, MpqHeader *header)
 {
@@ -360,34 +284,6 @@ bool find_header(const FileReader &f, MpqHeader *header)
 
 	return false;
 }
-
-void read_block_table(MpqHeader *header, void *data)
-{
-	byte *ptr = (byte *)data + header->block_table_offset + ((int64_t)(header->block_table_offset_high) << 32);
-	uint32_t key = hash_string("(block table)", HashTypeTableOffset);
-	int32_t len = header->block_table_entries * sizeof(BlockTableEntry);
-	BlockTableEntry *block_table = new BlockTableEntry[header->block_table_entries];
-	memcpy(block_table, ptr, len);
-	decrypt_data(block_table, len, key);
-	delete [] block_table;
-}
-
-void read_hash_table(MpqHeader *header, void *data)
-{
-
-}
-
-void read_het_table(MpqHeader *header, void *data)
-{
-	ExtendedTableHeader *p = (ExtendedTableHeader *)data;
-	int a = 10;
-}
-
-void read_bet_table(MpqHeader *header, void *data)
-{
-
-}
-
 
 FileReader::FileReader()
 	: _file(INVALID_HANDLE_VALUE)
@@ -516,128 +412,264 @@ void verify_file_idx(const HetTable *het_header, uint8_t *file_indices)
 		files.insert(file_idx);
 		int aa = 0;
 	}
-
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+
+struct MpqLoader {
+	MpqLoader();
+	bool load(const char *filename);
+	int32_t find_file(const char *filename);
+	void load_file(int32 idx, uint32 *file_pos, uint32 *file_size, uint32 *compressed_size, uint32 *flags, uint32 *unknown);
+
+//private:
+	// Different types of hashes to make with hash_string
+	enum HashType {
+		HashTypeTableOffset = 0,
+		HashTypeMethodA = 1,
+		HashTypeMethodB = 2,
+		HashTypeBlockTable = 3,
+	};
+
+	void init_crypt_table();
+	void decrypt_data(void *lpbyBuffer, uint32_t dwLength, uint32_t dwKey);
+	uint32_t hash_string(const char *lpszString, uint32_t dwHashType);
+
+	FileReader f;
+
+	HetTable *_het_header;
+	BetTable *_bet_header;
+	uint8 *_file_indices;
+	uint8 *_bet_hashes;
+	uint8 *_het_hashes;
+	uint8 *_bet_file_table;
+	scoped_array<byte> _het_data;
+	scoped_array<byte> _bet_data;
+	scoped_array<BlockTableEntry> _block_table;
+	scoped_array<HashTableEntry> _hash_table;
+
+	uint32 _crypt_table[0x500];
+	//uint8;
+};
+
+MpqLoader::MpqLoader()
+	: _het_header(nullptr)
+	, _bet_header(nullptr)
+	, _file_indices(nullptr)
+	, _bet_hashes(nullptr)
+	, _het_hashes(nullptr)
+	, _bet_file_table(nullptr)
 {
 	init_crypt_table();
+}
 
-	// todo: build some kind of windowed wrapper on top of this..
-	FileReader f;
-	if (!f.open("expansion3.mpq"))
-		return 1;
+uint32_t MpqLoader::hash_string(const char *lpszString, uint32_t dwHashType)
+{
+	uint32_t seed1 = 0x7FED7FEDL;
+	uint32_t seed2 = 0xEEEEEEEEL;
+	while (*lpszString) {
+		int ch = toupper(*lpszString++);
 
-	MpqHeader header;
-	if (!find_header(f, &header))
-		return 1;
+		seed1 = _crypt_table[(dwHashType * 0x100) + ch] ^ (seed1 + seed2);
+		seed2 = ch + seed1 + seed2 + (seed2 << 5) + 3;
+	}
+	return seed1;
+}
 
-	if (header.header_size != sizeof(MpqHeader))
-		return 1;
+void MpqLoader::init_crypt_table()
+{
+	// The encryption and hashing functions use a number table in their procedures.
+	// This table must be initialized before the functions are called the first time.
 
-	// only support cataclysm
-	if (header.format_version != 3)
-		return 1;
+	uint32_t seed = 0x00100001;
 
-	// read the block table
-	int bt_size = header.block_table_entries * sizeof(BlockTableEntry);
+	for (int index1 = 0; index1 < 0x100; index1++) {
+		for (int index2 = index1, i = 0; i < 5; i++, index2 += 0x100) {
+			uint32_t temp1, temp2;
 
-	scoped_array<BlockTableEntry> block_table(new BlockTableEntry[header.block_table_entries]);
-	f.read_ofs(block_table.get(), ((uint64_t)header.block_table_offset_high << 32) + header.block_table_offset, bt_size, NULL);
-	uint32_t bt_key = hash_string("(block table)", HashTypeBlockTable);
-	decrypt_data(block_table.get(), bt_size/4, bt_key);
+			seed  = (seed * 125 + 3) % 0x2AAAAB;
+			temp1 = (seed & 0xFFFF) << 0x10;
 
-	// read the hash table
-	int ht_size = header.hash_table_entries * sizeof(HashTableEntry);
-	scoped_array<HashTableEntry> hash_table(new HashTableEntry[header.hash_table_entries]);
-	f.read_ofs(hash_table.get(), ((uint64_t)header.hash_table_offset_high << 32) + header.hash_table_offset, ht_size, NULL);
-	uint32_t ht_key = hash_string("(hash table)", HashTypeBlockTable);
-	decrypt_data(hash_table.get(), ht_size/4, ht_key);
+			seed  = (seed * 125 + 3) % 0x2AAAAB;
+			temp2 = (seed & 0xFFFF);
 
-	// read the het table
-	if (!header.het_table_offset64)
-		return 1;
-
-	scoped_array<byte> het_data(new byte[(uint32_t)header.het_table_size64]);
-	f.read_ofs(het_data.get(), header.het_table_offset64, (uint32_t)header.het_table_size64, NULL);
-	if (!verify_md5(het_data.get(), (int)header.het_table_size64, header.md5_het_table))
-		return 1;
-
-	const HetTable *het_header = (HetTable *)het_data.get();
-	decrypt_data(het_data.get() + sizeof(ExtendedTableHeader), het_header->dwDataSize, hash_string("(hash table)", HashTypeBlockTable));
-	if (het_header->dwDataSize != het_header->dwTableSize)
-		return 1;
-
-	// set up pointers to the variable length data after the header
-	uint8_t *het_hashes = het_data.get() + sizeof(HetTable);
-	uint8_t *file_indices = het_hashes + het_header->dwHashTableSize;
-
-	// read the bet table
-	if (!header.bet_table_offset64)
-		return 1;
-
-	scoped_array<byte> bet_data(new byte[(uint32_t)header.bet_table_size64]);
-	f.read_ofs(bet_data.get(), header.bet_table_offset64, (uint32_t)header.bet_table_size64, NULL);
-	if (!verify_md5(bet_data.get(), (int)header.bet_table_size64, header.md5_bet_table))
-		return 1;
-
-	const BetTable *bet_header = (BetTable *)bet_data.get();
-	decrypt_data(bet_data.get() + sizeof(ExtendedTableHeader), bet_header->dwDataSize, hash_string("(block table)", HashTypeBlockTable));
-	if (bet_header->dwDataSize != bet_header->dwTableSize)
-		return 1;
-	DWORD *bet_flags = (DWORD *)(bet_data.get() + sizeof(BetTable));
-	uint8_t *bet_file_table = (uint8_t *)bet_flags + bet_header->dwFlagCount * sizeof(DWORD);
-	uint8_t *bet_hashes = bet_file_table + (bet_header->dwTableEntrySize * bet_header->dwFileCount + 7) / 8;
-
-	uint32_t c = 2, b = 1;
-	const char *listfile = "(listfile)";
-	hashlittle2(listfile, strlen(listfile), &c, &b);
-	uint64_t h = c + (((uint64_t)b) << 32);
-
-// mask the hash if needed (and set highest bit to 1)
-	uint64_t and_mask = fill_bits<uint64_t>(het_header->dwHashEntrySize);
-	h &= and_mask;
-	h |= (uint64_t)1 << (het_header->dwHashEntrySize - 1);
-
-	// het uses the highest 8 bits, bet uses rest
-	uint8_t het_hash = (uint8_t)(h >> (het_header->dwHashEntrySize - 8));
-	uint64_t bet_hash = h & fill_bits<uint64_t>(het_header->dwHashEntrySize - 8);
-
-	verify_file_idx(het_header, file_indices);
-	verify_file_table(bet_header, bet_file_table);
-
-	for (uint32_t i = 0 ; i < het_header->dwFileCount; ++i) {
-		uint64_t bb = packed_bits<uint64_t>(bet_hashes, i * bet_header->dwTotalBetHashSize, bet_header->dwBetHashSize);
-		if (bb == bet_hash) {
-			int a = 10;
+			_crypt_table[index2] = (temp1 | temp2);
 		}
 	}
+}
+
+void MpqLoader::load_file(int32 idx, uint32 *file_pos, uint32 *file_size, uint32 *compressed_size, uint32 *flags, uint32 *unknown)
+{
+	*file_pos = packed_bits<uint32>(_bet_file_table, _bet_header->dwTableEntrySize * idx + _bet_header->dwBitIndex_FilePos, _bet_header->dwFilePosBits);
+	*file_size = packed_bits<uint32>(_bet_file_table, _bet_header->dwTableEntrySize * idx + _bet_header->dwBitIndex_FileSize, _bet_header->dwFileSizeBits);
+	*compressed_size = packed_bits<uint32>(_bet_file_table, _bet_header->dwTableEntrySize * idx + _bet_header->dwBitIndex_CompressedSize, _bet_header->dwCompressedSizeBits);
+	*flags = packed_bits<uint32>(_bet_file_table, _bet_header->dwTableEntrySize * idx + _bet_header->dwBitIndex_FlagIndex, _bet_header->dwFlagsBits);
+	*unknown = packed_bits<uint32>(_bet_file_table, _bet_header->dwTableEntrySize * idx + _bet_header->dwBitIndex_Unknown, _bet_header->dwUnknownBits);
+}
+
+int32_t MpqLoader::find_file(const char *filename)
+{
+	uint32_t c = 2, b = 1;
+	hashlittle2(filename, strlen(filename), &c, &b);
+	uint64_t h = c + (((uint64_t)b) << 32);
+
+	// mask the hash if needed (and set highest bit to 1)
+	uint64_t and_mask = fill_bits<uint64_t>(_het_header->dwHashEntrySize);
+	h &= and_mask;
+	h |= (uint64_t)1 << (_het_header->dwHashEntrySize - 1);
+
+	// het uses the highest 8 bits, bet uses rest
+	uint8_t het_hash = (uint8_t)(h >> (_het_header->dwHashEntrySize - 8));
+	uint64_t bet_hash = h & fill_bits<uint64_t>(_het_header->dwHashEntrySize - 8);
 
 	int org_idx, idx;
-	org_idx = idx = h % het_header->dwHashTableSize;
+	org_idx = idx = h % _het_header->dwHashTableSize;
 	bool found = false;
 	while (true) {
-		byte h = het_hashes[idx];
+		byte h = _het_hashes[idx];
 		if (h == het_hash) {
 			// get the file index
-			uint32_t file_idx = packed_bits<uint32_t>(file_indices, idx * het_header->dwTotalIndexSize, het_header->dwIndexSize);
-			uint64_t bb = packed_bits<uint64_t>(bet_hashes, file_idx * bet_header->dwTotalBetHashSize, bet_header->dwBetHashSize);
+			uint32_t file_idx = packed_bits<uint32_t>(_file_indices, idx * _het_header->dwTotalIndexSize, _het_header->dwIndexSize);
+			uint64_t bb = packed_bits<uint64_t>(_bet_hashes, file_idx * _bet_header->dwTotalBetHashSize, _bet_header->dwBetHashSize);
 			if (bb == bet_hash) {
-				found = true;
-				break;
+				return file_idx;
 			}
 		}
 
-		idx = (idx + 1) % het_header->dwHashTableSize;
+		idx = (idx + 1) % _het_header->dwHashTableSize;
 		if (idx == org_idx)
 			break;
 	}
 
-	if (!found) {
-		// bad things..
+	return -1;
+}
+
+
+void MpqLoader::decrypt_data(void *lpbyBuffer, uint32_t dwLength, uint32_t dwKey)
+{
+	uint32_t *lpdwBuffer = (uint32_t *)lpbyBuffer;
+	uint32_t seed = 0xEEEEEEEEL;
+	uint32_t ch;
+
+	dwLength /= sizeof(uint32_t);
+
+	while(dwLength-- > 0) {
+		seed += _crypt_table[0x400 + (dwKey & 0xFF)];
+		ch = *lpdwBuffer ^ (dwKey + seed);
+
+		dwKey = ((~dwKey << 0x15) + 0x11111111L) | (dwKey >> 0x0B);
+		seed = ch + seed + (seed << 5) + 3;
+
+		*lpdwBuffer++ = ch;
+	}
+}
+
+bool MpqLoader::load(const char *filename)
+{
+	// todo: build some kind of windowed wrapper on top of this..
+	if (!f.open(filename))
+		return false;
+
+	MpqHeader header;
+	if (!find_header(f, &header))
+		return false;
+
+	if (header.header_size != sizeof(MpqHeader))
+		return false;
+
+	// only support cataclysm
+	if (header.format_version != 3)
+		return false;
+
+	// read the block table
+	int bt_size = header.block_table_entries * sizeof(BlockTableEntry);
+
+	_block_table.reset(new BlockTableEntry[header.block_table_entries]);
+	f.read_ofs(_block_table.get(), ((uint64_t)header.block_table_offset_high << 32) + header.block_table_offset, bt_size, NULL);
+	uint32_t bt_key = hash_string("(block table)", HashTypeBlockTable);
+	decrypt_data(_block_table.get(), bt_size/4, bt_key);
+
+	// read the hash table
+	int ht_size = header.hash_table_entries * sizeof(HashTableEntry);
+	_hash_table.reset(new HashTableEntry[header.hash_table_entries]);
+	f.read_ofs(_hash_table.get(), ((uint64_t)header.hash_table_offset_high << 32) + header.hash_table_offset, ht_size, NULL);
+	uint32_t ht_key = hash_string("(hash table)", HashTypeBlockTable);
+	decrypt_data(_hash_table.get(), ht_size/4, ht_key);
+
+	// read the het table
+	if (!header.het_table_offset64)
+		return false;
+
+	_het_data.reset(new byte[(uint32_t)header.het_table_size64]);
+	f.read_ofs(_het_data.get(), header.het_table_offset64, (uint32_t)header.het_table_size64, NULL);
+	if (!verify_md5(_het_data.get(), (int)header.het_table_size64, header.md5_het_table))
+		return false;
+
+	_het_header = (HetTable *)_het_data.get();
+	decrypt_data(_het_data.get() + sizeof(ExtendedTableHeader), _het_header->dwDataSize, hash_string("(hash table)", HashTypeBlockTable));
+	if (_het_header->dwDataSize != _het_header->dwTableSize)
+		return false;
+
+	// set up pointers to the variable length data after the header
+	_het_hashes = _het_data.get() + sizeof(HetTable);
+	_file_indices = _het_hashes + _het_header->dwHashTableSize;
+
+	// read the bet table
+	if (!header.bet_table_offset64)
+		return false;
+
+	_bet_data.reset(new byte[(uint32_t)header.bet_table_size64]);
+	f.read_ofs(_bet_data.get(), header.bet_table_offset64, (uint32_t)header.bet_table_size64, NULL);
+	if (!verify_md5(_bet_data.get(), (int)header.bet_table_size64, header.md5_bet_table))
+		return false;
+
+	_bet_header = (BetTable *)_bet_data.get();
+	decrypt_data(_bet_data.get() + sizeof(ExtendedTableHeader), _bet_header->dwDataSize, hash_string("(block table)", HashTypeBlockTable));
+	if (_bet_header->dwDataSize != _bet_header->dwTableSize)
+		return 1;
+	DWORD *bet_flags = (DWORD *)(_bet_data.get() + sizeof(BetTable));
+	_bet_file_table = (uint8_t *)bet_flags + _bet_header->dwFlagCount * sizeof(DWORD);
+	_bet_hashes = _bet_file_table + (_bet_header->dwTableEntrySize * _bet_header->dwFileCount + 7) / 8;
+
+	return true;
+}
+
+uint32 ofs = 0;
+extern "C"
+{
+	unsigned int read_buf(char *buf, unsigned int *size, void *param)
+	{
+		memcpy(buf, (uint8 *)param + ofs, *size);
+		ofs += *size;
+		return *size;
 	}
 
+	void write_buf(char *buf, uint32 *size, void *param)
+	{
 
+	}
+
+};
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	MpqLoader loader;
+	if (!loader.load("expansion3.mpq"))
+		return 1;
+
+	int idx = loader.find_file("(listfile)");
+	if (idx == -1)
+		return 1;
+
+	uint32 filesize, filepos, compressed_size, flags, unknown;
+	loader.load_file(idx, &filepos, &filesize, &compressed_size, &flags, &unknown);
+
+	scoped_array<byte> tmp(new byte[compressed_size]);
+	loader.f.read_ofs(tmp.get(), filepos, compressed_size, NULL);
+
+	scoped_array<byte> exploded(new byte[filesize]);
+	TDcmpStruct buf;
+	ZeroMemory(&buf, sizeof(buf));
+	explode(read_buf, write_buf, (char *)&buf, (void *)tmp.get());
 
 
 	return 0;
